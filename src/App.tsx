331924b7a62459ab4badd5fe456e07b33b7eeaa3
Alaa Eddine Ourmassi <aeourmassi@gmail.com>
2024-06-11 11:58:23 +0100

    \fix extentions issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import "./index.css";
 import Footer from "./Footer.tsx";
 import Header from "./Header.jsx";
-import SkillsSection from "./SkillsSection.jsx";
-import Projects from "./Projects.jsx";
-import Introduction from "./Introduction.jsx";
-import CurrentActivities from "./CurrentActivities.jsx";
+import Introduction from './Introduction.tsx';
+import Projects from './Projects.tsx';
+import SkillsSection from './SkillsSection.tsx';
+import CurrentActivities from './CurrentActivities.tsx';
 export default function App() {
   return (
     <div className="bg-[#080e1c]">
       <Header />
       <Introduction />
-      <Projects/>
-      <SkillsSection/>
+      <Projects />
+      <SkillsSection />
       <CurrentActivities />
       <Footer />
     </div>
